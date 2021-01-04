1fe5a3191a1e4d55445ea21800893420cad5a6a3
CGIARAD\CCastiblanco <c.castiblanco@cgiar.org>
2021-01-04 16:37:50 -0500

    Delete files sendtoslack

--- ETLs/config/sendtoslack.bat
+++
@@ -1,9 +0,0 @@
-# Slack Notification
-payload='payload={"text": "Error in ETL `'"${Internal.Job.Name}"'`.'"${error}"'"}'
-curl \
-    -H "Accept: application/json" \
-    -X POST \
-    --data-urlencode "${payload}" \
-    https://hooks.slack.com/services/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
-
-# exit 0
--- ETLs/config/sendtoslack0.bat
+++
@@ -1,5 +0,0 @@
-rem # Slack Notification
-rem payload='payload={"text": "Error in ETL `'"${Internal.Job.Name}"'`.'"${error}"'"}'
-curl -H "Accept: application/json" -X POST --data-urlencode 'payload={"text": "Test in ETL "}' https://hooks.slack.com/services/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
-
-rem exit 0
