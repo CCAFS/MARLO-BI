bf1e34bae49a17873c651cb9ba94d450001d9405
CGIARAD\CCastiblanco <c.castiblanco@cgiar.org>
2021-01-15 14:04:15 -0500

    Phase by cube change for funding sources cube.

--- Tables/04misc/Insert_bi_phase_by_cubes.sql
+++ Tables/04misc/Insert_bi_phase_by_cubes.sql
@@ -2,7 +2,7 @@ insert into bi_phase_by_cubes (cube_name, phase_name, phase_year)
 values ("deliverables", "AR", 2021);
 
 insert into bi_phase_by_cubes (cube_name, phase_name, phase_year)
-values ("fundingsources", "POWB", 2020);
+values ("fundingsources", "POWB", 2021);
 
 insert into bi_phase_by_cubes (cube_name, phase_name, phase_year)
 values ("projectsubmission", "AR", 2021);
