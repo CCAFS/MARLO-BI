9aff9def0abfa8b5413bbc524675cf1f32af0f9b
CGIARAD\CCastiblanco <c.castiblanco@cgiar.org>
2021-04-22 08:37:36 -0500

    Change Role name and Intersect group institutions to create a role_name_group

--- Tables/02stg/gendim/CR_stg_intersect_group_institutions.sql
+++ Tables/02stg/gendim/CR_stg_intersect_group_institutions.sql
@@ -1,8 +1,9 @@
-drop table if exists stg_intersect_group_institutions;
+drop table if exists `stg_intersect_group_institutions`;
 
-create table stg_intersect_group_institutions
-(institution_acr_name_group text,
-institution_name_group text,
-acronym_group text,
-ids_group varchar(200) NOT NULL
+create table `stg_intersect_group_institutions`
+(`institution_acr_name_group` text,
+`institution_name_group` text,
+`acronym_group` text,
+`role_name_group` text,
+`ids_group` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- Tables/02stg/gendim/CR_stg_pre_group_institutions.sql
+++ Tables/02stg/gendim/CR_stg_pre_group_institutions.sql
@@ -8,5 +8,6 @@ create table stg_pre_group_institutions
 `institution_acr_name_group` text,
 `institution_name_group` text,
 `acronym_group` text,
+`role_name_group` text,
 `ids_group` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
