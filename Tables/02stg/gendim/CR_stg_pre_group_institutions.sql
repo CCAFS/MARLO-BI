dbe53b6a6b422760d35a3168edade3d796184e03
CGIARAD\CCastiblanco <c.castiblanco@cgiar.org>
2021-04-29 15:51:36 -0500

    Changes to manage partner institutions and donor/funder institutions in the intersect table

--- Tables/02stg/gendim/CR_stg_pre_group_institutions.sql
+++ Tables/02stg/gendim/CR_stg_pre_group_institutions.sql
@@ -9,5 +9,14 @@ create table stg_pre_group_institutions
 `institution_name_group` text,
 `acronym_group` text,
 `role_name_group` text,
+`partner_institution_acr_name_group` text,
+`partner_institution_name_group` text,
+`partner_acronym_group` text,
+`direct_donor_institution_acr_name_group` text,
+`direct_donor_institution_name_group` text,
+`direct_donor_acronym_group` text,
+`original_donor_institution_acr_name_group` text,
+`original_donor_institution_name_group` text,
+`original_donor_acronym_group` text,
 `ids_group` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
