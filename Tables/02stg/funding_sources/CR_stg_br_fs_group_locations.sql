a6c1d7d8066f6471f11879ddc64c7f34cb25e892
CGIARAD\CCastiblanco <c.castiblanco@cgiar.org>
2021-05-12 07:56:13 -0500

    Delete scripts related to intersect tables for fs tables. The intersect tables are generic now.

--- Tables/02stg/funding_sources/CR_stg_br_fs_group_locations.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists stg_br_fs_group_locations;
-
-create table stg_br_fs_group_locations
-(
-`funding_source_id` bigint(20) NOT NULL,
-`loc_element_id` bigint(20) NOT NULL,
-`id_phase` bigint(20) DEFAULT NULL,
-`id_fs_intersect_group_loc` bigint(20) 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
