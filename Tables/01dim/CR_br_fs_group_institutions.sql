0e55992cf1ac07b97d134fb3d385f0bfe5218b60
CGIARAD\CCastiblanco <c.castiblanco@cgiar.org>
2021-08-24 15:13:39 -0500

    Deleting sql scripts of tables that are not being used.

--- Tables/01dim/CR_br_fs_group_institutions.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists br_fs_group_institutions;
-
-create table br_fs_group_institutions
-(
-`fk_id_fs_intersect_group_inst` bigint(20) 
-,`fk_id_ext_partner` bigint(20) 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
