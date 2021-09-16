c9139b158f2dc143525b1dbda3f9cd38634136b5
CGIARAD\CCastiblanco <c.castiblanco@cgiar.org>
2021-09-15 21:50:09 -0500

    Adding foreign keys to the tables: br_group_locations, br_group_milestones, pr_group_oicrs, br_group_owners, br_group_proj, br_group_reg_programs, br_group_sdg, br_group_smo_org_types, br_group_srf_cci, br_group_srf_targets

--- Tables/01dim/CR_br_group_srf_cci.sql
+++ Tables/01dim/CR_br_group_srf_cci.sql
@@ -5,6 +5,8 @@ create table br_group_srf_cci
 `fk_id_intersect_group_srf_cci` bigint(20) 
 ,`fk_id_srf_cci` bigint(20) 
 ,`is_primary` int(1) DEFAULT 0
-, `is_primary_description` varchar(4)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+, `is_primary_description` varchar(4), 
+CONSTRAINT `br_group_srf_cci_ibfk_01` FOREIGN KEY (`fk_id_intersect_group_srf_cci`) REFERENCES `dim_intersect_group_srf_cci` (`pk_id_intersect_group_srf_cci`) ON DELETE RESTRICT ON UPDATE RESTRICT, 
+CONSTRAINT `br_group_srf_cci_types_ibfk_02` FOREIGN KEY (`fk_id_srf_cci`) REFERENCES `dim_srf_cci` (`pk_id_srf_cci`) ON DELETE RESTRICT ON UPDATE RESTRICT  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
