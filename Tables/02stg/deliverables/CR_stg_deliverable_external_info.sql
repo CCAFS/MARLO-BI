b353d65c2c9218f32f0d4040d713a0c5a15cb2dd
Manuel Almanzar <m.r.almanzar@cgiar.org>
2021-11-03 09:15:07 -0500

    New deliverables dimension. Project submission using a phase parameter

--- Tables/02stg/deliverables/CR_stg_deliverable_external_info.sql
+++ Tables/02stg/deliverables/CR_stg_deliverable_external_info.sql
@@ -1,7 +1,6 @@
-drop table if exists stg_deliverable_metadata_external_sources ;
+drop table if exists stg_deliverable_external_info ;
 
-CREATE TABLE `stg_deliverable_metadata_external_sources` (
-  `id` bigint(20) NOT NULL ,
+CREATE TABLE `stg_deliverable_external_info` (
   `deliverable_id` bigint(20) DEFAULT NULL,
   `phase_id` bigint(20) DEFAULT NULL,
   `source` text,
@@ -16,13 +15,11 @@ CREATE TABLE `stg_deliverable_metadata_external_sources` (
   `journal_name` text,
   `volume` text,
   `pages` text,
-  `is_active` tinyint(1) NOT NULL DEFAULT '1',
-  `active_since` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `created_by` bigint(20) NOT NULL,
-  `create_date` timestamp NULL DEFAULT NULL,
   `gardian_findability` text,
   `gardian_accessibility` text,
   `gardian_interoperability` text,
   `gardian_reusability` text,
   `gardian_title` text
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
