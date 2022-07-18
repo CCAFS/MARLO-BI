7601b16893e93e462cb20d45b74d9ccafe713f50
ghernandez1849 <g.hernandez@cgiar.org>
2022-07-18 07:46:15 -0500

    Project Cube Info relationship

---
+++ Tables/02stg/info_relationship/stg_cube_dim_identifier.sql
@@ -0,0 +1,14 @@
+drop table if exists  `stg_cube_dim_identifier`;
+
+CREATE TABLE marlobi.stg_cube_dim_identifier
+(
+  id BIGINT NOT NULL 	
+, table_name VARCHAR(64)
+, src_table VARCHAR(64)
+, des_table VARCHAR(64)
+, src_column VARCHAR(64)
+, des_column VARCHAR(64)
+, cube_name VARCHAR(64)
+, dimension_identifier VARCHAR(64)
+, PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
