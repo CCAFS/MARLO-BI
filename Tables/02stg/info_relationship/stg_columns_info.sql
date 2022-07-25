ea68fa00445b177bf6b4e7a6b3ce13fc44c6299d
ghernandez1849 <g.hernandez@cgiar.org>
2022-07-25 09:50:35 -0500

    update script stg_column_info

--- Tables/02stg/info_relationship/stg_columns_info.sql
+++ Tables/02stg/info_relationship/stg_columns_info.sql
@@ -3,9 +3,6 @@ drop table if exists  `stg_columns_info`;
 CREATE TABLE stg_columns_info
 (
   pk_id_column_info BIGINT NOT NULL auto_increment
-, version INT
-, date_from DATETIME
-, date_to DATETIME
 , column_name VARCHAR(64)
 , table_name VARCHAR(64)
 , column_type MEDIUMTEXT
